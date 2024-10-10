dd0b1d5145a328dd289cefd5123aebf5c92daf77
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-10-10 23:25:21 +0800

    Implement data fetching in the backend

---
+++ GitHubUser.js
@@ -0,0 +1,56 @@
+import axios from "axios";
+
+class GitHubUser {
+  static #apiUrl = 'https://api.github.com/users/'
+  static async fetch(username) {
+    try {
+      const response = await axios.get(GitHubUser.#apiUrl + username)
+      return new GitHubUser(response.data)
+    } catch (error) {
+      if (error.response.status === 404) {
+        return new GitHubUser()
+      } else {
+        throw error
+      }
+    }
+  }
+
+  constructor(data) {
+    this.data = data
+    this.exists = Boolean(data)
+  }
+
+  get info() {
+    return {
+      name: this.data.name || this.data.login,
+      username: '@' + this.data.login,
+      avatar: this.data.avatar_url,
+      profile: this.data.html_url,
+      dateJoined: this.format(this.data.created_at),
+      bio: this.data.bio,
+      repos: this.data.public_repos,
+      followers: this.data.followers,
+      following: this.data.following,
+      location: this.data.location,
+      blog: this.data.blog,
+      company: this.data.company,
+      twitter: this.data.twitter_username,
+    }
+  }
+
+  /**
+   * Format a date into something like "26 Jan 2011"
+   * @param {Date | string} date - The date to format
+   * @param {object} options     - See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat/DateTimeFormat#options
+   * @returns {string}
+   */
+  format(date, { dateStyle = 'medium' } = {}) {
+    if (typeof date === 'string') {
+      date = new Date(date)
+    }
+    const options = { dateStyle, ...arguments[1] }
+    return new Intl.DateTimeFormat('en-GB', options).format(date)
+  }
+}
+
+export default GitHubUser
--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 import express from 'express'
+import GitHubUser from './GitHubUser.js'
 
 const app = express()
 const port = 3000
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => {
-  res.render('index', { title: 'devfinder' })
+app.get('/', async (req, res) => {
+  try {
+    const user = await GitHubUser.fetch('octocat')
+    res.render('index', { user: JSON.stringify(user.info) })
+  } catch (error) {
+    console.log(error)
+    res.sendStatus(500)
+  }
+})
+
+app.get('/:username', async (req, res) => {
+  try {
+    const user = await GitHubUser.fetch(req.params.username)
+    if (user.exists) {
+      res.render('index', { user: JSON.stringify(user.info) })
+    } else {
+      res.render('index', { error: 'No matching users.' })
+    }
+  } catch (error) {
+    console.log(error)
+    res.sendStatus(500)
+  }
 })
 
 app.listen(port, () => {
